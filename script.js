047605a3253a424f917d43cba78c3bbb1657143b
Sisonke10-byt <sisonke.boyilani@capaciti.org.za>
2024-12-02 12:11:38 +0200

    Add anchor link
    Added anchor link to allow smooth scrolling on all sections

--- script.js
+++ script.js
@@ -5,29 +5,29 @@
  * Date: November 2024
  */
 
-// Wait for the DOM to be fully loaded before attaching event listeners
+//HAMBURGER MENU
+
 document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.getElementById('hamburger');
     const closeIcon = document.getElementById('closeIcon');
     const navLinks = document.getElementById('navLinks');
     const hamburgerIcon = document.getElementById('hamburgerIcon');
 
-    // Toggle the navigation menu and icon when the hamburger icon is clicked
     hamburger.addEventListener('click', function() {
-        navLinks.classList.add('active'); // Show the mobile menu
-        hamburgerIcon.style.display = 'none'; // Hide the hamburger icon
-        closeIcon.style.display = 'block'; // Show the close icon
+        navLinks.classList.add('active'); 
+        hamburgerIcon.style.display = 'none'; 
+        closeIcon.style.display = 'block'; 
     });
 
-    // Close the navigation menu and icon when the close icon is clicked
+    
     closeIcon.addEventListener('click', function() {
-        navLinks.classList.remove('active'); // Hide the mobile menu
-        hamburgerIcon.style.display = 'block'; // Show the hamburger icon
-        closeIcon.style.display = 'none'; // Hide the close icon
+        navLinks.classList.remove('active'); 
+        hamburgerIcon.style.display = 'block'; 
+        closeIcon.style.display = 'none'; 
     });
 });
 
-
+//CONTACTS POPUP
 
 const popup = document.getElementById('contactPopup');
 const closePopup = document.getElementById('closePopup');
@@ -50,7 +50,7 @@ window.addEventListener('click', function(event) {
     }
 });
 
-
+//CHATBOT
     // Initialize chatbot state
     let chatbotOpen = false; // Boolean to check if chatbot is open
     let chatHistory = []; // Array to store user and bot messages
